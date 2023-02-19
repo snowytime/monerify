488c407b5b1a70e915c903a7fec96f097ae115b6
Snaer <87044139+snowytime@users.noreply.github.com>
2023-02-18 23:55:36 -0700

    ⚡️ auto patch

--- src/parser/index.test.ts
+++ src/parser/index.test.ts
@@ -1,49 +1,48 @@
+import test from "ava";
+
 import { Crypt, Transaction } from "../index.js";
 import { parseArguments } from "./index.js";
 
-describe("Testing the argument parser", () => {
-    describe("Error cases", () => {
-        it("should throw an error if the type is not defined", () => {
-            expect(() => parseArguments({} as any)).to.throw();
-        });
-        it("should throw an error if the type is not a member of the Transaction enum", () => {
-            expect(() => parseArguments({ type: "foo" } as any)).to.throw();
-        });
-        it("should throw an error if any required fields are missing", () => {
-            expect(() => parseArguments({ type: Transaction.Purchase } as any)).to.throw();
-        });
+test("should throw an error if the type is not defined", (t) => {
+    t.throws(() => parseArguments({} as any));
+});
+test("should throw an error if the type is not a member of the Transaction enum", (t) => {
+    t.throws(() => parseArguments({ type: "foo" } as any));
+});
+test("should throw an error if any required fields are missing", (t) => {
+    t.throws(() => parseArguments({ type: Transaction.Purchase } as any));
+});
+
+test("should return the type and rest where all non required / optional fields are removed", (t) => {
+    const parsed = parseArguments({
+        type: Transaction.Purchase,
+        orderId: "123",
+        amount: "1.00",
+        pan: "1234567890123456",
+        expdate: "1234",
+        cryptType: Crypt.SslEnabled,
+        test: "bar",
+    } as any);
+    t.deepEqual(parsed, {
+        type: Transaction.Purchase,
+        orderId: "123",
+        amount: "1.00",
+        pan: "1234567890123456",
+        expdate: "1234",
+        cryptType: Crypt.SslEnabled,
     });
-    describe("Success cases", () => {
-        it("should return the type and rest where all non required / optional fields are removed", () => {
-            const parsed = parseArguments({
-                type: Transaction.Purchase,
-                orderId: "123",
-                amount: "1.00",
-                pan: "1234567890123456",
-                expdate: "1234",
-                cryptType: Crypt.SslEnabled,
-                test: "bar",
-            } as any);
-            expect(parsed).to.deep.equal({
-                type: Transaction.Purchase,
-                orderId: "123",
-                amount: "1.00",
-                pan: "1234567890123456",
-                expdate: "1234",
-                cryptType: Crypt.SslEnabled,
-            });
-        });
-        it("should remove all additional fields that are not required or optional", () => {
-            const parsed = parseArguments({
-                type: Transaction.Purchase,
-                orderId: "123",
-                amount: "1.00",
-                pan: "1234567890123456",
-                expdate: "1234",
-                cryptType: Crypt.SslEnabled,
-                foo: "bar",
-            } as any);
-            expect(parsed).to.not.have.property("foo");
-        });
-    });
+});
+test("should remove all additional fields that are not required or optional", (t) => {
+    const parsed = parseArguments({
+        type: Transaction.Purchase,
+        orderId: "123",
+        amount: "1.00",
+        pan: "1234567890123456",
+        expdate: "1234",
+        cryptType: Crypt.SslEnabled,
+        foo: "bar",
+    } as any);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    t.falsy(parsed.foo);
 });
